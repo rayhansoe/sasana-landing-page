5b2d1c6b983863fbcf4c1418a66424b4da63ec44
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-11-05 05:23:27 +0700

    add Layout.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import dynamic from 'next/dynamic'
+
+const Layout = dynamic(() => import('../components/Layout'))
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
+	)
 }
 
 export default MyApp
