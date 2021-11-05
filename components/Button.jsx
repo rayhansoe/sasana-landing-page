2eef7a245ce4f700ea7935abf583284b731a92b9
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-11-06 04:58:28 +0700

    add new option and variants or type for button.

--- components/Button.jsx
+++ components/Button.jsx
@@ -4,24 +4,38 @@ import styles from '../styles/Button.module.scss'
 
 const Button = ({ href, children, type, color }) => {
 	return (
-		<div className={`${styles.button} ${type === 'text' ? styles.text : ''}`}>
+		<div
+			className={`${styles.button} ${
+				type === 'text'
+					? styles.text
+					: type === 'outline'
+					? styles.outline
+					: type === 'course'
+					? styles.course
+					: type === 'disable'
+					? styles.disable
+					: ''
+			}`}>
 			<Link href={href}>
 				<a className={styles.link}>
 					<p style={{ color: `${color && color}` }}>{children}</p>
-					{console.log(color)}
-					<Image
-						className={styles.arrow}
-						src={
-							type === 'text' && !color
-								? '/icon/right-arrow-v2.svg'
-								: color === '#fff'
-								? '/icon/right-arrow-v3.svg'
-								: '/icon/right-arrow.svg'
-						}
-						alt='right arrow'
-						width={16}
-						height={16}
-					/>
+					{type === 'outline' || type === 'course' || type === 'disable' ? (
+						''
+					) : (
+						<Image
+							className={styles.arrow}
+							src={
+								type === 'text' && !color
+									? '/icon/right-arrow-v2.svg'
+									: color === '#fff'
+									? '/icon/right-arrow-v3.svg'
+									: '/icon/right-arrow.svg'
+							}
+							alt='right arrow'
+							width={16}
+							height={16}
+						/>
+					)}
 				</a>
 			</Link>
 		</div>
