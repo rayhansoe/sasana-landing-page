230ee6b9c06ad8088f23315c5e6284bbb85e39aa
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-11-05 11:25:10 +0700

    change component name and add Footer on Layout.

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,14 +2,16 @@ import dynamic from 'next/dynamic'
 import styles from '../styles/Layout.module.scss'
 
 const NavBar = dynamic(() => import('./NavBar'))
+const Footer = dynamic(() => import('./Footer'))
 
-const index = ({ children }) => {
+const Layout = ({ children }) => {
 	return (
 		<div className={styles.container}>
 			<NavBar />
 			<main>{children}</main>
+			<Footer />
 		</div>
 	)
 }
 
-export default index
+export default Layout
