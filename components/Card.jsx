875bb0f13390bf271e661b2855b75d07821d6767
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-11-06 02:43:28 +0700

    add more options and variants or type for card ui.

--- components/Card.jsx
+++ components/Card.jsx
@@ -4,22 +4,36 @@ import styles from '../styles/Card.module.scss'
 
 const Button = dynamic(() => import('./Button'))
 
-const Card = ({ src, width, height, alt, href, service, title, text, layout }) => {
+const Card = ({ src, width, height, alt, href, service, title, text, layout, type }) => {
 	return (
-		<div className={`${styles.card} ${layout && styles.active}`}>
-			<div className={styles.imageContainer}>
-				<Image width={width} height={height} src={src} alt={alt} />
-			</div>
-			<h5 className={styles.headingX5}>{title}</h5>
-			<p className={styles.myText}>{text}</p>
-			<div className={styles.info}>
-				<h6 className={styles.headingX6}>Layanan yang Tersedia:</h6>
-				<p className={`${styles.myText} ${styles.v2}`}>{service}</p>
-				<Button href={href} type='text' color={layout && '#fff'}>
-					Selengkapnya
-				</Button>
-			</div>
-		</div>
+		<>
+			{type === 'activity' ? (
+				<div className={`${styles.card} ${layout && styles.active} ${type && styles.activity}`}>
+					<div className={styles.image}>
+						<Image src={src} width={width} height={height} alt={alt} layout='fill' />
+					</div>
+					<div className={styles.desc}>
+						<h5 className={styles.headingX5}>{title}</h5>
+						<p className={styles.myText}>{text}</p>
+					</div>
+				</div>
+			) : (
+				<div className={`${styles.card} ${layout && styles.active}`}>
+					<div className={styles.imageContainer}>
+						<Image width={width} height={height} src={src} alt={alt} />
+					</div>
+					<h5 className={styles.headingX5}>{title}</h5>
+					<p className={styles.myText}>{text}</p>
+					<div className={styles.info}>
+						<h6 className={styles.headingX6}>Layanan yang Tersedia:</h6>
+						<p className={`${styles.myText} ${styles.v2}`}>{service}</p>
+						<Button href={href} type='text' color={layout && '#fff'}>
+							Selengkapnya
+						</Button>
+					</div>
+				</div>
+			)}
+		</>
 	)
 }
 
