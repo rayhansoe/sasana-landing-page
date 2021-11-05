a5a1cf736ac7b4ee969a9983681904f748f8332c
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-11-06 01:34:46 +0700

    add more options and variants for button.

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,18 +2,25 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Button.module.scss'
 
-const Button = ({ href, children }) => {
+const Button = ({ href, children, type, color }) => {
 	return (
-		<div className={styles.button}>
+		<div className={`${styles.button} ${type === 'text' ? styles.text : ''}`}>
 			<Link href={href}>
 				<a className={styles.link}>
-					<p>{children}</p>
+					<p style={{ color: `${color && color}` }}>{children}</p>
+					{console.log(color)}
 					<Image
 						className={styles.arrow}
-						src='/icon/right-arrow.svg'
+						src={
+							type === 'text' && !color
+								? '/icon/right-arrow-v2.svg'
+								: color === '#fff'
+								? '/icon/right-arrow-v3.svg'
+								: '/icon/right-arrow.svg'
+						}
 						alt='right arrow'
-						width={24}
-						height={24}
+						width={16}
+						height={16}
 					/>
 				</a>
 			</Link>
